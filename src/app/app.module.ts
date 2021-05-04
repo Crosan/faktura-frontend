0818f3bd2453da44d957d420f200065d69a60c74
Roald Simonsen <31620386+archStant@users.noreply.github.com>
2021-05-04 14:21:38 +0200

    Fixes update madness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,53 @@
-import {
-  MatInputModule,
-  MatCardModule,
-  MatTableModule,
-  MatGridListModule,
-  MatSelectModule,
-  MatIconModule,
-  MatNativeDateModule,
-  MatDatepickerModule,
-  MatAutocompleteModule,
-  MatExpansionModule,
-  MatButtonModule,
-  MatTabsModule,
-  MatSnackBarModule,
-  MatSnackBar,
-  MAT_DIALOG_DATA,
-  MatSnackBarContainer
-} from '@angular/material';
+// import {
+//   MatInputModule,
+//   MatCardModule,
+//   MatTableModule,
+//   MatGridListModule,
+//   MatSelectModule,
+//   MatIconModule,
+//   MatNativeDateModule,
+//   MatDatepickerModule,
+//   MatAutocompleteModule,
+//   MatExpansionModule,
+//   MatButtonModule,
+//   MatTabsModule,
+//   MatSnackBarModule,
+//   MatSnackBar,
+//   MAT_DIALOG_DATA,
+//   MatSnackBarContainer
+// } from '@angular/material';
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+// import {MatNativeDateModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDialogModule, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { MaterialFileInputModule } from 'ngx-material-file-input';
 import localeDA from '@angular/common/locales/da';
@@ -37,7 +69,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler, LOCALE_ID } from '@angular/core';
-import { MatDialogModule } from '@angular/material';
+// import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -57,6 +89,7 @@ import { ParsingComponent } from './parsing/parsing.component';
 import { RekvirentComponent } from './rekvirenter/rekvirenter.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { EANdialogComponent } from './rekvirenter/EANdialog/EANdialog.component';
+import { MatNativeDateModule } from '@angular/material/core';
 // import { EANdialogComponent } from './rekvirenter/EANdialog/EANdialog.component'
 
 registerLocaleData(localeDA);
@@ -112,7 +145,7 @@ const routes: Routes = [
     RekvirentComponent,
     EANdialogComponent,
   ],
-  entryComponents: [EANdialogComponent,MatSnackBarContainer],
+  entryComponents: [EANdialogComponent],//,MatSnackBarContainer],
   imports: [
     BrowserModule,
     FormsModule,
